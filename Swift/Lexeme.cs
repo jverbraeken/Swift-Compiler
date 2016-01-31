a8fc507fda662a2bb55ea990b7bc55bfc75688e3
Joost Verbraeken <j.verbraeken@student.tudelft.nl>
2016-01-31 19:58:23 +0100

    Initial commit

---
+++ Swift/Lexeme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swift
+{
+    class Lexeme
+    {
+        public string line;
+        public Lexeme(string test)
+        {
+            line = test;
+        }
+    }
+}
