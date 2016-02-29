1352025581e434c4bb5a7f608c1a0c589d34aa55
Joost Verbraeken <j.verbraeken@hotmail.com>
2016-02-29 11:58:11 +0100

    Ignore

--- Swift/AST Nodes/ASTNode.cs
+++ Swift/AST Nodes/ASTNode.cs
@@ -11,20 +11,13 @@ namespace Swift
     {
         private Global.ASTType type; //The type of the node, eg a function, a string, a class
         private List<ASTNode> children; //The arguments for a function call
-<<<<<<< HEAD
-        private LineContext context;
-        private string name;
-        private ASTNode returnType;
-        private Table scope;
-        private Exp exp1; //For comparisons and assignments (lhs)
-        private Exp exp2; //rhs
-=======
         private LineContext context; //For debugging, gives the programmer an idea where the node is located in the original source code
         private string name; //The name of the function, the string, ...
         private ASTNode returnType; //The return type of a function
         private Table scope; //The scope every variable, function, etc. is made in
         private int assemblyLocation; //The location the node is located when compiled to assembly (mostly used for constant variables)
->>>>>>> 184c33e6450c3373b762a61135d8d3396a846c62
+        private Exp exp1; //For comparisons and assignments (lhs)
+        private Exp exp2; //rhs
 
         /// <summary>
         /// Should not be called directly, but only by its children
